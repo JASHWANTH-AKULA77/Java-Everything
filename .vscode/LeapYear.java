b4244f1ee00b94ad4bd4084608f6c1033bccd147
Jashwanth <jashwanth0206@gmail.com>
2025-05-12 18:40:06 +0530

    Second commit

---
+++ .vscode/LeapYear.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class LeapYear {
+    
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the year : ");
+		year = sc.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))  {
+			System.out.println(year + " Is a leap year");
+			
+		}
+		else {
+			System.out.println(year +"is not a leap year");
+		}
+		
+
+	}
+
+}
+
