93ea0a1248e155413633d1e266efd4cb51ca2e91
Jashwanth <jashwanth0206@gmail.com>
2025-05-14 20:51:42 +0530

    third commit

---
+++ .vscode/BinaryVisualization.java
@@ -0,0 +1,15 @@
+public class BinaryVisualization {
+                     
+    public static void main(String[] args) {
+        int a=4;
+        int b=7;
+        int res=a|b;
+        String s1 = Integer.toBinaryString(a);
+        String s2 = Integer.toBinaryString(b);
+        String op = Integer.toBinaryString(res);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(op);
+        System.out.println(res);
+}
+                     }
