55460ea54c6f506a222cad497ad896dd2e096984
Jashwanth <jashwanth0206@gmail.com>
2025-05-18 21:37:55 +0530

    Method usage

---
+++ .vscode/printConcatenation.java
@@ -0,0 +1,11 @@
+public class printConcatenation {
+    public static void main(String[] args) {
+        String res=printconcat("Hello", "world!");
+        System.out.println(res);
+
+    }
+    static String printconcat(String s1,String s2){
+        String res=s1+s2;
+        return res;
+    }
+}
