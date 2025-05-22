bef97bba6293f754e0e1385f5ff9183bb83c8e20
Jashwanth <jashwanth0206@gmail.com>
2025-05-22 19:53:20 +0530

    oop

---
+++ oopl/oopex1.java
@@ -0,0 +1,13 @@
+package oopl;
+
+public class oopex1 {
+    public static void main(String[] args) {
+    student s1=new student();
+    s1.study();
+    }
+}
+class student{
+    void study(){
+        System.out.println("Student is studying");
+    }
+}
