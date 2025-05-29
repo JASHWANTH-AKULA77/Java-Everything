efeb063978d178c997a626d3485ca24834487dea
Jashwanth <jashwanth0206@gmail.com>
2025-05-29 19:31:37 +0530

    java

---
+++ oopl/oopex2.java
@@ -0,0 +1,11 @@
+public class oopex2 {
+   public static void main(String[] args) {
+    company n1=new company();
+    n1.employee();
+   } 
+}
+class company{
+    void employee(){
+    System.out.println("Employee is working");
+    }
+}
