b85b67a3ba3975d893a2cc19c36480117d134f8b
Jashwanth <jashwanth0206@gmail.com>
2025-05-31 19:35:59 +0530

    Java

--- Basic_programs/LeapYear.java
+++ Basic_programs/LeapYear.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class LeapYear {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int year;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the year : ");
--- oopl/oopex2.java
+++ oopl/oopex2.java
@@ -1,16 +1,19 @@
 public class oopex2 {
    public static void main(String[] args) {
-    company n1=new company();
-    n1.employee();
-    n1.hr();
+        company n1=new company();
+        n1.employee();
+        n1.hr();
+        n1.mangement();
    } 
 }
 class company{
     void employee(){
-    System.out.println("Employees");
-
+        System.out.println("Employees");
     }
     void hr(){
         System.out.println("Hrs will be present");
+    }
+    void mangement(){
+        System.out.println("Mangement will be functional");
     }
 }
